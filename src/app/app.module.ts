894e111e60eab6df4ef1e558843209b7dfbc1f96
SoumyaGanesh12 <revathyganesh98@gmail.com>
2021-06-10 22:42:10 +0530

    header footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+ 		HeaderComponent,
+   		FooterComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		FormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
