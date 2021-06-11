ee67e18e48905ccaf8b810ae642b560e634189c1
KaustubhD <kaustubhd287@gmail.com>
2021-06-11 14:57:08 +0530

    Added query params to pass

--- src/app/components/email-phone/email-phone.component.ts
+++ src/app/components/email-phone/email-phone.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-email-phone',
-  templateUrl: './email-phone.component.html',
-  styleUrls: ['./email-phone.component.css']
+	selector: 'app-email-phone',
+	templateUrl: './email-phone.component.html',
+	styleUrls: ['./email-phone.component.css']
 })
 export class EmailPhoneComponent implements OnInit {
-  @Input() item = "";
-  constructor(private router : Router) { 
-  }
+	public email: String | undefined
+	public phone: String | undefined
 
-  ngOnInit(): void {
-  }
+	constructor(private router: Router, private route: ActivatedRoute) {
+		route.queryParams.subscribe(params => {
+			// TODO: Remove this if
+			if(!params.id) {
+				router.navigate(['/email-phone'], {
+					queryParams: {
+						email: btoa("abc@gmail.com"),
+						phone: btoa("9944336677")
+					}
+				})
+			}
+			this.email = params.email && atob(params.email)
+			this.phone = params.phone && atob(params.phone)
+		})
+	}
 
-  onSend(){
-    this.router.navigate(['/otp']);
-  }
+	ngOnInit(): void {
+	}
+
+	onSend(choice: Number) {
+			if(choice == 1) {
+				// this.http.post("/otp/generate", {email: this.email})
+			}
+			else if (choice == 2){
+				// this.http.post("/otp/generate", {phone: this.phone})
+			}
+			else {
+				// Do nothing
+				return
+			}
+			this.router.navigate(['/otp']);
+	}
 }
