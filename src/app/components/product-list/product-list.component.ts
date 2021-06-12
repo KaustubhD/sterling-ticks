514e97e602c0123f622965c41c7183321b326c7f
Arnab <noonevs@gmail.com>
2021-06-12 11:58:38 +0530

    In progress

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products: Product[] = [
+    {
+      "name" : "Product 1",
+      "price" : 500,
+      "rating": 5,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name" : "Product 2",
+      "price" : 800,
+      "rating": 5,
+      "brand": "Helio",
+      "img": ""
+    },
+    {
+      "name" : "Product 2",
+      "price" : 800,
+      "rating": 5,
+      "brand": "Helio",
+      "img": ""
+    },
+    {
+      "name" : "Product 2",
+      "price" : 800,
+      "rating": 5,
+      "brand": "Helio",
+      "img": ""
+    },
+    {
+      "name" : "Product 2",
+      "price" : 800,
+      "rating": 5,
+      "brand": "Helio",
+      "img": ""
+    },
+    {
+      "name" : "Product 2",
+      "price" : 800,
+      "rating": 5,
+      "brand": "Helio",
+      "img": ""
+    },
+    {
+      "name" : "Product 2",
+      "price" : 800,
+      "rating": 5,
+      "brand": "Helio",
+      "img": ""
+    },
+    {
+      "name" : "Product 2",
+      "price" : 800,
+      "rating": 5,
+      "brand": "Helio",
+      "img": ""
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.fetchProducts();
   }
 
+  fetchProducts(){
+    console.log("Fetching Products");
+  }
 }
