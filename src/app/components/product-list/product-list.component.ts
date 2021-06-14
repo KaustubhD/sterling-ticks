02683e5f258f3e27e20eb16f5f785b54ea051de5
Arnab <noonevs@gmail.com>
2021-06-14 09:16:39 +0530

    stash

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -14,9 +14,120 @@ export class ProductListComponent implements OnInit {
   page: number = 1;
   maxItem: number = 12;
   brandSelect: boolean = false;
-  products: Product[] = [];
-  brands: Brand[] = [];
-  categories: string[] = [];
+  products: Product[] = [
+    {
+      "name": "Product 1",
+      "price": 500,
+      "rating": 1,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 2",
+      "price": 120,
+      "rating": 2,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 3",
+      "price": 6078,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 4",
+      "price": 973,
+      "rating": 5,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 5",
+      "price": 800,
+      "rating": 5,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 6",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 7",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 8",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 9",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    }, {
+      "name": "Product 10",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 11",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 12",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 13",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    }, {
+      "name": "Product 14",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 15",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    },
+    {
+      "name": "Product 16",
+      "price": 800,
+      "rating": 4,
+      "brand": "Helio",
+      "img": "testimg.webp"
+    }
+  ];
+  brands: Brand[] = [{name: "helio",description: "Premium watch brand. Estd. 1980, London"},{name: "Titan",description: ""},{name: "Breguet",description: ""}];
+  categories: string[] = ["Rich", "Expensive","Sell Kidneys"];
   lastPage: number;
   aBrand: Brand;
   constructor(private service: ProductService, private router: Router, private aroute: ActivatedRoute) { }
