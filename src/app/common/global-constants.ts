0f4a4cc57f3ebbbbbcc5741bd08c03adb91e847c
Arnab <noonevs@gmail.com>
2021-06-15 13:06:21 +0530

    minor update

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,6 +1,6 @@
 export class GlobalConstants{
     public static title : string = 'sterling-ticks';
-    public static apiUrl : string = 'http://localhost:8085/sterling-ticks';
+    public static apiUrl : string = '/sterling-ticks';
 
 	public static URLS = {
 		OTP_GENERATE: GlobalConstants.apiUrl + "/otp/generate",
