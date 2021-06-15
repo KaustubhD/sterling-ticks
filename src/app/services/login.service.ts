149408c368f8abf208f1ca6054ff9cce46537d14
KaustubhD <kaustubhd287@gmail.com>
2021-06-16 01:32:15 +0530

    Adds roles to userModel and IsAdmin method to login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,6 +3,7 @@ import { CredentialModel as user } from '../components/models/credential.model';
 import { HttpClient } from '@angular/common/http';
 import { GlobalConstants } from '../common/global-constants';
 import { UserModel } from '../components/models/user.model';
+import { Roles } from '../components/models/role.model';
 
 @Injectable({
   providedIn: 'root'
@@ -20,8 +21,13 @@ export class LoginService {
   authenticate(user: user) {
     return this.http.post<UserModel>(GlobalConstants.URLS.LOGIN, user);
   }
+  
   saveAuthenticatedUser(user: UserModel) {
     this.loggedIn = true;
     this.authenticatedUser = user;
+  }
+
+  isAdmin(): boolean {
+	return this.getAuthenticatedUser().roles.some(({role}) => role == Roles.AD)
   }
 }
