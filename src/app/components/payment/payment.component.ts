5ed45f83f833330cbc50b1456020745c58126ec1
SoumyaGanesh12 <revathyganesh98@gmail.com>
2021-06-23 23:03:36 +0530

    Order success page, Checks card validity, payment and cart page fixes

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -5,6 +5,8 @@ import { PaymentModel } from '../models/payment.model';
 import { PlaceOrderModel } from '../models/placeorder.model';
 import { AddressModel } from '../models/address.model';
 import { Router } from '@angular/router';
+import { formatDate } from '@angular/common';
+
 
 @Component({
 	selector: 'app-payment',
@@ -21,6 +23,10 @@ export class PaymentComponent implements OnInit {
 	year: number;
 	addressId: number;
 	order: PlaceOrderModel = new PlaceOrderModel();
+	currentDate: string;
+	currentYear: string;
+	currentMonth: string;
+	greater: boolean | undefined;
 
 	constructor(private service: LoginService, private cartService: CartItemsService, private router: Router) {
 		this.service.getAuthenticatedUser().subscribe(user => {
@@ -32,11 +38,17 @@ export class PaymentComponent implements OnInit {
 				}
 			});
 		});
+		this.currentDate = formatDate(new Date(), 'yyyy/MM/dd', 'en')
+		this.currentYear = this.currentDate.substr(0, 4);
+		this.currentMonth = this.currentDate.substr(5, 2);
+
 	}
 
 	ngOnInit(): void { }
 
 	onPaymentSuccess() {
+		let validDate = this.checkExpiryDate()
+		if(validDate){
 		this.payment.expiry = new Date(this.year, this.month)
 		this.cartService.saveCard(this.payment, this.name).then((res: any) => {
 			this.payment.paymentMethodId = res.id
@@ -47,16 +59,29 @@ export class PaymentComponent implements OnInit {
 			this.cartService.placeOrder(this.order).then((res: any) => {
 				let result: boolean = res.result;
 				this.paymentSuccess = result;
+				if(this.paymentSuccess == true){
+					this.router.navigate(['/success'])
+				}
 			}).catch((e) => {
 				throw e;
 			})
 		}).catch(() => {
 			this.paymentSuccess = false;
 		})
+		}
 	}
 
 	setAddress(address: AddressModel) {
 		this.addressId = address.id;
+	}
+
+	checkExpiryDate(){
+		if(isNaN(this.year) || isNaN(this.month)){
+			this.greater = false
+		}else{
+			this.greater = parseInt(this.currentYear) < this.year ? true: (parseInt(this.currentMonth) <= this.month) ? true : false;
+		}
+		return this.greater
 	}
 
 }
--- src/app/components/success/success.component.ts
+++ src/app/components/success/success.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../models/order.model';
+import { OrderService } from 'src/app/services/order.service';
+import { LoginService } from 'src/app/services/login.service';
+import { UserModel } from '../models/user.model';
 
 @Component({
   selector: 'app-success',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessComponent implements OnInit {
 
-  constructor() { }
+  orders: Order[] = [];
+  user: UserModel = new UserModel();
+  latestOrderIndex: number;
+  constructor(private service: OrderService, private authService: LoginService) { }
 
   ngOnInit(): void {
+    this.authService.getAuthenticatedUser().subscribe(user => {
+			this.user = user ;
+    });
+    this.service.getOrders(this.user.userName).then(data =>{
+      this.orders = data;
+    });
+
   }
 
 }
